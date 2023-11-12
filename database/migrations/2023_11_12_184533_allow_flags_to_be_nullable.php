380b10b918e550cd5674608155e38b833073d6b9
Christopher Pitt <cgpitt@gmail.com>
2023-11-12 20:47:26 +0200

    I hate myself

--- database/migrations/2023_11_12_184533_allow_flags_to_be_nullable.php
+++ database/migrations/2023_11_12_184533_allow_flags_to_be_nullable.php
@@ -8,14 +8,14 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('be_nullable', function (Blueprint $table) {
+        Schema::table('sprites', function (Blueprint $table) {
             $table->json('flags')->nullable(true)->change();
         });
     }
 
     public function down(): void
     {
-        Schema::table('be_nullable', function (Blueprint $table) {
+        Schema::table('sprites', function (Blueprint $table) {
             $table->json('flags')->nullable(false)->change();
         });
     }
