66e901699590b3103210dd4c6b6d3bf7dbc0625c
Christopher Pitt <cgpitt@gmail.com>
2023-12-09 19:11:29 +0200

    Update deps

--- resources/views/components/code-block.blade.php
+++ resources/views/components/code-block.blade.php
@@ -2,7 +2,7 @@
     'language',
 ])
 @if (env('TORCHLIGHT_ENABLED'))
-    <pre><x-torchlight-code :language="$language">{{ $slot }}</x-torchlight-code></pre>
+    <x-torchlight-code :language="$language">{{ $slot }}</x-torchlight-code>
 @else
     <code class="text-xs whitespace-pre-line w-full overflow-x-scroll">{{ $slot }}</code>
 @endif
