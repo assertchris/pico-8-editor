ad3e109dec2f6cbc5c680e3d72661dff85136d63
Christopher Pitt <cgpitt@gmail.com>
2023-12-10 22:33:34 +0200

    Bigger code, please

--- resources/views/components/code-block.blade.php
+++ resources/views/components/code-block.blade.php
@@ -1,12 +1,10 @@
 @props([
     'language',
 ])
-<div class="text-xs">
-    @php
-        print \Spatie\ShikiPhp\Shiki::highlight(
-            code: preg_replace('/<!--(.|\s)*?-->/', '', $slot),
-            language: $language,
-            theme: 'github-light',
-        );
-    @endphp
-</div>
+@php
+    print \Spatie\ShikiPhp\Shiki::highlight(
+        code: preg_replace('/<!--(.|\s)*?-->/', '', $slot),
+        language: $language,
+        theme: 'github-light',
+    );
+@endphp
